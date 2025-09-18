35b308fa1f522937a781a0ae313c19d1ba50ddd3
Qurbonoff11 <jonibekqurbonov570@gmail.com>
2025-09-18 13:15:43 +0500

    updates

--- js/script.js
+++ js/script.js
@@ -13,55 +13,53 @@ const STEP = 88 * 2;
 let x = 0;
 let isStart = false;
 
-// carni yangilash
+// car positions
 const carPosition = () => {
   car.style.transform = `translateX(${x}px)`;
 };
 
-// start tugmasi
+// start/stop function
 const startStop = () => {
     isStart = !isStart;
     startBtn.classList.toggle("start__button--active");
 };
 
+// start/stop button
 startBtn.addEventListener("click", startStop);
 
+// start/stop enter
 $HTML.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
     startStop();
   }
 })
 
-// tugmalar
+// car movement
 $HTML.addEventListener("keydown", (e) => {
   if (!isStart) return;
   console.log(e.key);
 
-  // elementlarning koordinatalari
+  // borders cordinates
   const carRect = car.getBoundingClientRect();
   const leftBorder = roadBorderLeft.getBoundingClientRect();
   const rightBorder = roadBorderRight.getBoundingClientRect();
 
-  // mashina markazi
+  // car center
   const carCenter = carRect.left + carRect.width / 2;
 
-  // chegaralar
+  // borders limits
   const minX = leftBorder.right + carRect.width / 2;
   const maxX = rightBorder.left - carRect.width / 2;
 
+  // car movement logic
   if (e.key === "ArrowRight") {
-    // o‘ngga yurmoqchi bo‘lsa va hali chegaradan chiqmagan bo‘lsa
     if (carCenter + STEP <= maxX) {
       x += STEP;
     }
   } else if (e.key === "ArrowLeft") {
-    // chapga yurmoqchi bo‘lsa va hali chegaradan chiqmagan bo‘lsa
     if (carCenter - STEP >= minX) {
       x -= STEP;
     }
-  } else if (e.key === " ") {
-    // bo‘sh joy tugmasi bosilsa
-    car.style.scale = "1.5";
   }
 
   carPosition();
